fc49ab9affb9b65522556da79a8978d6bdb9e562
Prabin Pun <prabin1punah@gmail.com>
2018-08-17 21:19:19 -0400

    commit

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -4,12 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp11
+namespace HomeWork2A
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Create a console application that will accept ten numbers between 0 and 100, and report their sum.
+            
+            int sum = 0;// Initializing  the sum as 0;
+            int userInput;//This variable holds the user entries
+            for (int i = 0; i < 10; i++)
+                {           
+                Console.Write("Enter a number between 1 and 100 : "); //string
+                userInput = int.Parse(Console.ReadLine());
+                sum = sum + userInput;
+            }
+            
+            Console.WriteLine("The sum is " + sum);
+            Console.ReadKey();
+            
+
         }
     }
 }
+
